877384b72738643fa8a373422772b24a3deb710f
liaodehui <1959956996@qq.com>
2020-04-10 21:48:11 +0800

    feat 迭代

--- tree/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/index.js
+++ tree/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/index.js
@@ -10,3 +10,21 @@
 //   worker(root)
 //   return arr[k - 1]
 // };
+
+// 迭代
+var kthLargest = function(root, k) {
+  let count = 1,stack = []
+  let cur = root
+  while(cur || stack.length) {
+    while(cur) {
+      stack.push(cur)
+      cur = cur.right
+    }
+    // let cur = stack.pop() // 报错：cur is not defined，因为未初始化就调用
+    cur = stack.pop()
+    if (count++ === k) {
+      return cur.val
+    }
+    cur = cur.left
+  }
+}
