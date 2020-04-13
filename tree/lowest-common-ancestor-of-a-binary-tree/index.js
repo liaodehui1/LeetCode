00b0f48d7bc0ceb150aaca43e5b6e3016f961412
liaodehui <1959956996@qq.com>
2020-04-13 23:50:54 +0800

    feat 父指针法

--- tree/lowest-common-ancestor-of-a-binary-tree/index.js
+++ tree/lowest-common-ancestor-of-a-binary-tree/index.js
@@ -1,3 +1,4 @@
+// 递归
 var lowestCommonAncestor = function(root, p, q) {
   let res = null
   function worker(node, p, q) {
@@ -13,4 +14,31 @@ var lowestCommonAncestor = function(root, p, q) {
   }
   worker(root, p, q)
   return res
+};
+
+// 父指针
+var lowestCommonAncestor = function(root, p, q) {
+  let map = new Map(), stack = [root]
+  map.set(root, null)
+  while(!map.has(p) || !map.has(q)) {
+    let node = stack.pop()
+    if(node.left) {
+      map.set(node.left, node)
+      stack.push(node.left)
+    }
+    if(node.right) {
+      map.set(node.right, node)
+      stack.push(node.right)
+    }
+  }
+  let pp = [p], parent = p
+  while(parent) {
+    parent = map.get(parent)
+    pp.push(parent)
+  }
+  parent = q
+  while(!pp.includes(parent)) {
+    parent = map.get(parent)
+  }
+  return parent
 };
